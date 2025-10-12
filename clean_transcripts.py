6928232b9e11ad849be2c79714d737551b9467da
Library Laptop <laptop@NUL-LENDLAP53.library.northwestern.edu>
2025-10-12 16:42:25 -0500

    Mark technical: add TODO for anonymization step

--- clean_transcripts.py
+++ clean_transcripts.py
@@ -6,3 +6,5 @@ print("Cleaning transcripts...")
 # TODO: Add actual cleaning steps for filler words or spacing later.
 
 # TODO: Accept a transcripts/ folder path via command-line argument.
+
+#TODO: Add anonymization step to replace participant names with pseudonyms.
